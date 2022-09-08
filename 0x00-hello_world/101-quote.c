9aef1656ee5d7a4958038e67bb8bfcbe37280569
Saint Light <miraclelight11@gmail.com>
2022-09-08 19:08:29 +0100

    added comment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/** 
+ * Using system instead of printf
+ * and put
+ * 
+ * **/
 
 int main(void)
 {
-	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+	system("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
