6d59820ffb0c227985d875ebd597ab90ccd205e5
Miracle11 <miraclelight11@gmail.com>
2022-09-13 19:00:44 +0100

    more debugging to 0 and 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
+#include <unistd.h>
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
-int main()
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
 {
-    printf("_putchar \n");
-    return 0;
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
