ff5bf4923643435f72170e81c38eee0bb6831a3b
miracle11 <miraclelight11@gmail.com>
2022-09-22 16:09:14 +0100

    Question 0 fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,24 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int srclen = 0, i = 0;
-char *temp = dest, *start = src;
-while (*src)
-{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
 	srclen++;
-	src++;
-}
-srclen++;
-if (n > srclen)
-	n = srclen;
-src = start;
-for (; i < n; i++)
-	*dest++ = *src++;
-return (temp);
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
 }
