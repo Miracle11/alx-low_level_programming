5ade0b1aa9a8e8c3cf02f199f6c2cd46469c94df
miracle11 <miraclelight11@gmail.com>
2022-09-26 20:38:13 +0100

    Still on question 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,21 +8,24 @@
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-char *startn = needle, *starth = haystack;
-while (*haystack)
-{
-	starth = haystack;
-	needle = startn;
-	while (*haystack == *needle)
+	char *startn = needle, *starth = haystack;
+
+	while (*haystack)
 	{
-		haystack++;
-		needle++;
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+		haystack = starth + 1;
 	}
-	if (*needle == '\0')
-		return (haystack);
-	haystack = starth + 1;
-}
-return (NULL);
+	return (NULL);
 }
